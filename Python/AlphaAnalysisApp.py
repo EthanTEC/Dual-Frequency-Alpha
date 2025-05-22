1133048680b28d62f1fed7d944c2199dcc500222
EthanTEC <ethan.predinchuk@thrusterenergy.com>
2025-05-22 10:55:15 -0600

    Full committing v1.0.1

--- Python/AlphaAnalysisApp.py
+++ Python/AlphaAnalysisApp.py
@@ -21,12 +21,12 @@ from textwrap import wrap
 from tempfile import NamedTemporaryFile
 
 # Application version
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 # URL where the current version info is stored (should return JSON with {'version': 'x.y.z', 'url': 'http://.../AlphaAnalysisApp.py'})
 UPDATE_INFO_URL = "https://raw.githubusercontent.com/EthanTEC/Dual-Frequency-Alpha/refs/heads/main/Python/update_info.json"
 
 # Appearance setup
-ctk.set_appearance_mode('System')
+ctk.set_appearance_mode('dark')
 ctk.set_default_color_theme('blue')
 
 # Determine base path so that "Images/…" can be loaded both in dev and once bundled:
